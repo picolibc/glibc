b2cd3aeb9c0a2fbd03b37065e7630a48571bb6b8
Fangrui Song <maskray@google.com>
2022-01-08 00:17:48 -0800

    elf: Support DT_RELR relative relocation format [BZ #27924]
    PIE and shared objects usually have many relative relocations. In
    2017/2018, SHT_RELR/DT_RELR was proposed on
    https://groups.google.com/g/generic-abi/c/bX460iggiKg/m/GxjM0L-PBAAJ
    ("Proposal for a new section type SHT_RELR") and is a pre-standard. RELR
    usually takes 3% or smaller space than R_*_RELATIVE relocations. The
    virtual memory size of a mostly statically linked PIE is typically 5~10%
    smaller.
    
    To protect mysterious crash when a DT_RELR using object runs with older
    glibc, GNU ld's -z pack-relative-relocs adds a version dependency on
    libc.so.6's GLIBC_ABI_DT_RELR version node. This change defines
    GLIBC_ABI_DT_RELR in libc.so.6.
    
    ---
    
    Notes I will not include in the submitted commit:
    
    Available on https://sourceware.org/git/?p=glibc.git;a=shortlog;h=refs/heads/maskray/relr
    
    "pre-standard": even Solaris folks are happy with the refined generic-abi
    proposal. Cary Coutant will apply the change
    https://sourceware.org/pipermail/libc-alpha/2021-October/131781.html
    
    This patch is simpler than Chrome OS's glibc patch and makes ELF_DYNAMIC_DO_RELR
    available to all ports. I don't think the current glibc implementation
    supports ia64 in an ELFCLASS32 container. That said, the style I used is
    works with an ELFCLASS32 container for 64-bit machine if ElfW(Addr) is
    64-bit.
    
    * Chrome OS folks have carried a local patch since 2018 (latest version:
      https://chromium.googlesource.com/chromiumos/overlays/chromiumos-overlay/+/refs/heads/main/sys-libs/glibc/files/local/glibc-2.32).
      I.e. this feature has been battle tested.
    * Android bionic supports 2018 and switched to DT_RELR==36 in 2020.
    * The Linux kernel has supported CONFIG_RELR since 2019-08
      (https://git.kernel.org/linus/5cf896fb6be3effd9aea455b22213e27be8bdb1d).
    * A musl patch (by me) exists but is not applied:
      https://www.openwall.com/lists/musl/2019/03/06/3
    * rtld-elf from FreeBSD 14 will support DT_RELR.
    
    I believe upstream glibc should support DT_RELR to benefit all Linux
    distributions. I filed some feature requests to get their attention:
    
    * Gentoo: https://bugs.gentoo.org/818376
    * Arch Linux: https://bugs.archlinux.org/task/72433
    * Debian https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=996598
    * Fedora https://bugzilla.redhat.com/show_bug.cgi?id=2014699
    
    As of linker support (to the best of my knowledge):
    
    * LLD support DT_RELR.
    * https://chromium.googlesource.com/chromiumos/overlays/chromiumos-overlay/+/refs/heads/main/sys-devel/binutils/files/
      has a gold patch.
    * GNU ld feature request https://sourceware.org/bugzilla/show_bug.cgi?id=27923
    
    Tested on aarch64 and x86_64.
    
    Changes from v1 (https://sourceware.org/pipermail/libc-alpha/2021-October/131768.html)
    * Fix style, simplify code
    * Improve test
    
    Changes from v2
    * Add GLIBC_ABI_DT_RELR version node similar to HJ Lu's GLIBC_ABI_VERSION_GEN2
      https://sourceware.org/pipermail/libc-alpha/2021-November/133386.html

---
+++ elf/libc-abi-dt-relr.c
@@ -0,0 +1,21 @@
+/* Placeholder definition for GLIBC_ABI_DT_RELR node.
+   Copyright (C) 2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#ifdef SHARED
+const char __libc_abi_dt_relr_placeholder = 0;
+#endif
