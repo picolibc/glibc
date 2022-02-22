e61ac505ea99114c725e22679883389b8cb626ba
Keith Packard <keithp@keithp.com>
2022-02-21 16:27:22 -0800

    picolibc: More hacks
    Support malloc testing.
    Fix value of M_PERTURB.
    Set arch-specific TININESS_AFTER_ROUNDING value
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- picolibc/hacks.h
+++ picolibc/hacks.h
@@ -29,7 +29,7 @@ static inline int chdir(const char *a) { return 0; }
 #define stat64 stat
 #define fstat64 fstat
 
-#define M_PERTURB 0
+#define M_PERTURB -6
 
 struct rlimit { int rlim_cur, rlim_max; };
 
@@ -52,7 +52,19 @@ typedef void (*sighandler_t)(int);
 #define fputs_unlocked fputs
 #define sleep(x)
 
+static inline long sysconf(int x) { switch (x) { case 8: return 0x1000; default: return -1; } }
+
+#define xsysconf sysconf
+#define xmalloc malloc
+
+#define powerof2(x)	(((x) & ((x)-1)) == 0)
+
+#define loff_t off_t
+
 #define __USE_ISOC11
+
+/* XXX arch-specific */
+#define TININESS_AFTER_ROUNDING 0
 
 #include "../misc/sys/cdefs.h"
 #include <math.h>
